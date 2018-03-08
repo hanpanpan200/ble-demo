0297d8f453df2d33b52068fd581cfd385dc44d4a
Panpan Han <hanpanpan200@gmail.com>
2018-03-08 17:09:38 +0800

    feat: add different beacon uuids

--- src/navigationExample/IOSBeacon.js
+++ src/navigationExample/IOSBeacon.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { View, Text, DeviceEventEmitter, Platform } from 'react-native'
+import _ from 'lodash'
 import { Actions } from 'react-native-router-flux'
 import Beacons from 'react-native-beacons-manager'
 
 const region = {
   identifier: '',
-  uuid: 'B5B182C7-EAB1-4988-AA99-B5C1517008D9',
+  // uuid: 'B5B182C7-EAB1-4988-AA99-B5C1517008D9', // April Brother
+  // uuid: 'E2C56DB5-DFFB-48D2-B060-D0F5A71096E0', // Seekcy
+  uuid: 'E2C56DB5-DFFB-48D2-B060-D0F5A71096E0',
 }
 
 const isIOS = Platform.OS === 'ios'
@@ -15,9 +18,9 @@ export default class IOSBeacon extends React.Component {
     if (isIOS) {
       Beacons.requestWhenInUseAuthorization()
     }
-    // Beacons.startRangingBeaconsInRegion(region)
-    //   .then(() => console.log('startRanging successfully'))
-    //   .catch(error => console.log('error when startRangingBeaconsInRegion>>>>>', error))
+    Beacons.startRangingBeaconsInRegion(region)
+      .then(() => console.log('startRanging successfully'))
+      .catch(error => console.log('error when startRangingBeaconsInRegion>>>>>', error))
     Beacons.startMonitoringForRegion(region)
       .then(() => console.log('start monitoring successfully'))
       .catch(error => console.log('start monitoring error>>>', error))
@@ -27,15 +30,23 @@ export default class IOSBeacon extends React.Component {
       Beacons.shouldDropEmptyRanges(true)
     }
 
-    // this.beaconsDidRange = DeviceEventEmitter.addListener('beaconsDidRange', data => console.log('beaconsDidRange data>>>>>>>>>>', data))
+    this.beaconsDidRange = DeviceEventEmitter.addListener('beaconsDidRange', data => {
+      console.log('data>>>', data)
+      if (isIOS) {
+        console.log('beaconsDidRange data>>>>>>>>>>', _.get(data, 'beacons[0].accuracy'))
+      } else {
+        console.log('beaconsDidRange data>>>>>>>>>>', _.get(data, 'beacons[0].distance'))
+      }
+
+    })
     this.regionDidEnter = DeviceEventEmitter.addListener('regionDidEnter', data => console.log('regionDidEnter data>>>>>>: ', data))
     this.regionDidExit = DeviceEventEmitter.addListener('regionDidExit', data => console.log('regionDidExit data>>', data))
   }
 
   componentWillUnMount() {
-    // Beacons.stopRangingBeaconsInRegion(region)
-    //   .then(() => console.log('stop ranging beacons in region successfully'))
-    //   .catch(error => console.log('stop ranging beacons in region failed', error))
+    Beacons.stopRangingBeaconsInRegion(region)
+      .then(() => console.log('stop ranging beacons in region successfully'))
+      .catch(error => console.log('stop ranging beacons in region failed', error))
     Beacons.stopMonitoringForRegion(region)
       .then(() => console.log('stop monitoring successfully'))
       .catch(error => console.log('stop monitoring failed', error))
@@ -43,7 +54,7 @@ export default class IOSBeacon extends React.Component {
       Beacons.stopUpdatingLocation()
     }
     if (this.regionDidEnter) { this.regionDidEnter.remove() }
-    // if (this.beaconsDidRange) { this.beaconsDidRange.remove() }
+    if (this.beaconsDidRange) { this.beaconsDidRange.remove() }
     if (this.regionDidExit) { this.regionDidExit.remove() }
   }
 
